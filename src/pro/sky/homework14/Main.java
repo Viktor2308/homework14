e34030a1911eeda41f45d72fe965c011957f2d51
Viktor <viktor_23_08@mail.ru>
2023-01-13 14:15:15 +0100

    homework 17

--- src/pro/sky/homework14/Main.java
+++ src/pro/sky/homework14/Main.java
@@ -9,6 +9,7 @@ import pro.sky.homework14.transport.Car;
 import pro.sky.homework14.transport.Transport;
 import pro.sky.homework14.transport.Truck;
 
+
 public class Main {
     public static void main(String[] args) {
 
@@ -16,38 +17,43 @@ public class Main {
         TruckDriver truckDriver = new TruckDriver("Petr Petrov", true, 6);
         BusDriver busDriver = new BusDriver("Mixail Mihailov", true, 8);
 
-        Car carLadaGranta = new Car("Lada", "Granta", 1.7);
-        Car carAudiA8 = new Car("Audi", "A8 50 L TDI quattro", 3.0);
-        Car carBmwZ8 = new Car("BMW", "Z8", 3.0);
-        Car carMercedes = new Car("Mercedes", "A250", 2.5);
+        Car carLadaGranta = new Car("Lada", "Granta", 1.7, Car.CarBodyType.SEDAN);
+        Car carAudiA8 = new Car("Audi", "A8 50 L TDI quattro", 3.0, Car.CarBodyType.SEDAN);
+        Car carBmwX5 = new Car("BMW", "X5", 3.0, Car.CarBodyType.UNIVERSAL);
+        Car carMercedes = new Car("Mercedes", "A250", 2.5, Car.CarBodyType.NO_INFORMATION);
 
-        Truck truckKamaz = new Truck("Kamaz", "3116", 9);
-        Truck truckScania = new Truck("Scania", "G18", 8.5);
-        Truck truckVolvo = new Truck("Volvo", "Volvo Truck 8000", 8.2);
-        Truck truckMan = new Truck("Man", "Man 666", 9.9);
+        Truck truckKamaz = new Truck("Kamaz", "3116", 9, Truck.LoadCapacity.N1);
+        Truck truckScania = new Truck("Scania", "G18", 8.5, Truck.LoadCapacity.N2);
+        Truck truckVolvo = new Truck("Volvo", "Volvo Truck 8000", 8.2, Truck.LoadCapacity.N3);
+        Truck truckMan = new Truck("Man", "Man 666", 9.9, Truck.LoadCapacity.NO_INFORMATION);
 
-        Bus busVolvo = new Bus("Volvo", "Volvo 9500", 7.7);
-        Bus busScania = new Bus("Scania", "Scania CRUIZE", 8);
-        Bus busHyundai = new Bus("Hyundai", "Hyundai Universe", 8.5);
-        Bus busIkarus = new Bus("Ikarus", "Ikarus 7000", 7);
+        Bus busVolvo = new Bus("Volvo", "Volvo 9500", 7.7, Bus.PeopleCapacity.LARGE);
+        Bus busScania = new Bus("Scania", "Scania CRUIZE", 8, Bus.PeopleCapacity.ESPECIALLY_LARGE);
+        Bus busHyundai = new Bus("Hyundai", "Hyundai Universe", 8.5, Bus.PeopleCapacity.MEDIUM);
+        Bus busIkarus = new Bus("Ikarus", "Ikarus 7000", 7, Bus.PeopleCapacity.NO_INFORMATION);
 
-        carMercedes.participateInRace(carDriver);
+        carLadaGranta.printType();
+        truckKamaz.printType();
+        busIkarus.printType();
+        truckMan.printType();
 
-        truckKamaz.participateInRace(truckDriver);
-
-        busVolvo.participateInRace(busDriver);
-
-        transportAllMethod(busIkarus);
-        driverAllMethod(truckDriver);
-
-        busDriver.setDriveLicense(false);
-        busVolvo.participateInRace(busDriver);
-        driverAllMethod(busDriver);
+//        carMercedes.participateInRace(carDriver);
+//
+//        truckKamaz.participateInRace(truckDriver);
+//
+//        busVolvo.participateInRace(busDriver);
+//
+//        transportAllMethod(busIkarus);
+//        driverAllMethod(truckDriver);
+//
+//        busDriver.setDriveLicense(false);
+//        busVolvo.participateInRace(busDriver);
+//        driverAllMethod(busDriver);
 
 
     }
 
-    public static void transportAllMethod(Transport transport) {
+    public static void transportAllMethod(Transport<?> transport) {
 
         transport.startMovement();
         transport.pitStop();
--- src/pro/sky/homework14/transport/Truck.java
+++ src/pro/sky/homework14/transport/Truck.java
@@ -4,9 +4,58 @@ import pro.sky.homework14.driver.TruckDriver;
 
 
 public class Truck extends Transport<TruckDriver> {
+    private LoadCapacity loadCapacity;
 
-    public Truck(String mark, String model, double engineVolume) {
+    public Truck(String mark, String model, double engineVolume, LoadCapacity loadCapacity) {
         super(mark, model, engineVolume);
+        this.loadCapacity = loadCapacity;
+    }
+
+    public enum LoadCapacity {
+        N1(null, 3.5f),
+        N2(3.5f, 12f),
+        N3(12f, null),
+        NO_INFORMATION(null, null);
+
+
+        final Float minCapacity;
+        final Float maxCapacity;
+
+        public Float getMinCapacity() {
+            return minCapacity;
+        }
+
+        public Float getMaxCapacity() {
+            return maxCapacity;
+        }
+
+        LoadCapacity(Float minCapacity, Float maxCapacity) {
+            this.minCapacity = minCapacity;
+            this.maxCapacity = maxCapacity;
+
+        }
+
+        @Override
+        public String toString() {
+            if(minCapacity == null && maxCapacity == null){
+                return "No information about capacity";
+            }
+            if (minCapacity == null) {
+                return "Carrying capacity with a gross weight of up to " + maxCapacity + " tons.";
+            } else if (maxCapacity == null) {
+                return "Carrying capacity with a gross weight over " + minCapacity + " tons.";
+            } else {
+                return "Carrying capacity with a gross weight of over " + minCapacity + " to " + maxCapacity + " tons.";
+            }
+        }
+    }
+
+    public LoadCapacity getLoadCapacity() {
+        return loadCapacity;
+    }
+
+    public void setLoadCapacity(LoadCapacity loadCapacity) {
+        this.loadCapacity = loadCapacity;
     }
 
     @Override
@@ -17,6 +66,11 @@ public class Truck extends Transport<TruckDriver> {
         } else {
             System.out.println(driver.getName() + " don't drive, because don't have drive licence!");
         }
+    }
+
+    @Override
+    public void printType() {
+            System.out.println("Truck " + getMark() + ' ' + getModel() + ' ' + getLoadCapacity());
     }
 
     @Override
